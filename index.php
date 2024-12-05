41c4d563551d71e8f1f797d7dbae183e61590b7d
sandirusso2@gmail.com <sandirusso2@gmail.com>
2024-12-05 12:16:39 +0100

    Aggiunta la sezione "Recenti" nella home

--- index.php
+++ index.php
@@ -125,6 +125,82 @@ $main_categories = get_main_categories();
         <!-- Sezione degli Articoli con angoli stondati a destra e shadow -->
         <div class="articoli">
             <?php
+
+            // Sezione Ultime Notizie
+            $latest_posts = new WP_Query(array(
+                'posts_per_page' => 4,
+                'orderby' => 'date',
+                'order' => 'DESC'
+            ));
+
+            if ($latest_posts->have_posts()):
+                ?>
+                <section class="category-section latest-news">
+                    <div class="category-section-title">
+                        <div>
+                            <div>
+                                <div class="category_name latest-news">
+                                    Recenti
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="grid-home grid-home-2x2">
+                        <?php
+                        while ($latest_posts->have_posts()):
+                            $latest_posts->the_post();
+                            ?>
+                            <div class="card-home-other">
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php if (has_post_thumbnail()): ?>
+                                        <?php the_post_thumbnail('medium_large', array('class' => 'card_img')); ?>
+                                    <?php else: ?>
+                                        <div class="card_img_not_available">
+                                            <p>Immagine non disponibile</p>
+                                        </div>
+                                    <?php endif; ?>
+
+                                    <div class="card_background">
+                                        <h3 class="card_title">
+                                            <?php the_title(); ?>
+                                        </h3>
+                                </a>
+                                <p class="card_description">
+                                    <?php echo wp_trim_words(get_the_excerpt(), 20, '...'); ?>
+                                </p>
+                                <div class="card_info_author">
+                                    <div class="card_author">
+                                        <?php
+                                        $author_id = get_the_author_meta('ID');
+                                        $author_posts_url = get_author_posts_url($author_id);
+                                        ?>
+
+                                        <a href="<?php echo esc_url($author_posts_url); ?>" class="card_author-link">
+                                            <p class="card_author"><?php echo get_author_full_name_uppercase($author_id); ?></p>
+                                        </a>
+
+                                        <p class="card_author"><?php echo get_the_date(); ?></p>
+                                    </div>
+
+                                    <div class="card_author-space">
+                                        <a href="<?php echo esc_url($author_posts_url); ?>" class="card_author-avatar-link">
+                                            <?php echo get_avatar($author_id, 24, '', '', array('class' => 'card_author_avatar')); ?>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <?php
+                        endwhile;
+                        wp_reset_postdata();
+                        ?>
+            </div>
+            </section>
+            <?php
+            endif;
+
+
             foreach ($main_categories as $category):
                 // Verifica se la categoria esiste e ha l'ID
                 if (isset($category->term_id)):
@@ -139,100 +215,100 @@ $main_categories = get_main_categories();
                     // Mostra la sezione solo se ci sono post
                     if ($category_posts->have_posts()):
                         ?>
-                        <section class="category-section">
-                            <!-- Intestazione Categoria con Stile Specifico -->
-                            <div class="category-section-title">
+                    <section class="category-section">
+                        <!-- Intestazione Categoria con Stile Specifico -->
+                        <div class="category-section-title">
+                            <div>
                                 <div>
-                                    <div>
-                                        <a href="<?php echo esc_url(get_category_link($category->term_id)); ?>">
-                                            <div class="category_name <?php echo esc_attr($category->slug); ?>">
-                                                <?php echo esc_html($category->name); ?>
-                                            </div>
-                                        </a>
-                                    </div>
+                                    <a href="<?php echo esc_url(get_category_link($category->term_id)); ?>">
+                                        <div class="category_name <?php echo esc_attr($category->slug); ?>">
+                                            <?php echo esc_html($category->name); ?>
+                                        </div>
+                                    </a>
                                 </div>
                             </div>
+                        </div>
 
-                            <div class="grid-home grid-home-2x2"> <!-- Added grid-home-2x2 class -->
-                                <?php
-                                while ($category_posts->have_posts()):
-                                    $category_posts->the_post();
-                                    ?>
-                                    <!-- ALL CARDS ARE NOW card-home-other -->
-                                    <div class="card-home-other">
-                                        <a href="<?php the_permalink(); ?>">
-                                            <?php if (has_post_thumbnail()): ?>
-                                                <?php the_post_thumbnail('medium_large', array('class' => 'card_img')); ?>
-                                            <?php else: ?>
-                                                <div class="card_img_not_available">
-                                                    <p>Immagine non disponibile</p>
-                                                </div>
-                                            <?php endif; ?>
-
-                                            <div class="card_background">
-                                                <h3 class="card_title">
-                                                    <?php the_title(); ?>
-                                                </h3>
-                                        </a>
-                                        <p class="card_description">
-                                            <?php echo wp_trim_words(get_the_excerpt(), 20, '...'); ?>
-                                        </p>
-                                        <div class="card_info_author">
-                                            <div class="card_author">
-                                                <?php
-                                                // Recupera l'ID dell'autore
-                                                $author_id = get_the_author_meta('ID');
-
-                                                // Recupera il nome e il cognome dell'autore
-                                                $first_name = get_the_author_meta('first_name', $author_id);
-                                                $last_name = get_the_author_meta('last_name', $author_id);
-                                                $display_name = trim($first_name . ' ' . $last_name);
-
-                                                // Usa il nome utente come fallback
-                                                if (empty($display_name)) {
-                                                    $display_name = get_the_author_meta('user_login', $author_id);
-                                                }
-
-                                                // Ottieni l'URL della pagina dell'autore
-                                                $author_posts_url = get_author_posts_url($author_id);
-                                                ?>
-
-                                                <!-- Nome dell'autore cliccabile che reindirizza alla pagina dell'autore -->
-                                                <a href="<?php echo esc_url($author_posts_url); ?>" class="card_author-link">
-                                                    <p class="card_author"><?php echo get_author_full_name_uppercase($author_id); ?></p>
-                                                </a>
-
-                                                <!-- Data del post -->
-                                                <p class="card_author"><?php echo get_the_date(); ?></p>
+                        <div class="grid-home grid-home-2x2"> <!-- Added grid-home-2x2 class -->
+                            <?php
+                            while ($category_posts->have_posts()):
+                                $category_posts->the_post();
+                                ?>
+                                <!-- ALL CARDS ARE NOW card-home-other -->
+                                <div class="card-home-other">
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php if (has_post_thumbnail()): ?>
+                                            <?php the_post_thumbnail('medium_large', array('class' => 'card_img')); ?>
+                                        <?php else: ?>
+                                            <div class="card_img_not_available">
+                                                <p>Immagine non disponibile</p>
                                             </div>
+                                        <?php endif; ?>
 
-                                            <div class="card_author-space">
-                                                <!-- Avatar dell'autore cliccabile che reindirizza alla pagina dell'autore -->
-                                                <a href="<?php echo esc_url($author_posts_url); ?>" class="card_author-avatar-link">
-                                                    <?php echo get_avatar($author_id, 24, '', '', array('class' => 'card_author_avatar')); ?>
-                                                </a>
-                                            </div>
+                                        <div class="card_background">
+                                            <h3 class="card_title">
+                                                <?php the_title(); ?>
+                                            </h3>
+                                    </a>
+                                    <p class="card_description">
+                                        <?php echo wp_trim_words(get_the_excerpt(), 20, '...'); ?>
+                                    </p>
+                                    <div class="card_info_author">
+                                        <div class="card_author">
+                                            <?php
+                                            // Recupera l'ID dell'autore
+                                            $author_id = get_the_author_meta('ID');
+
+                                            // Recupera il nome e il cognome dell'autore
+                                            $first_name = get_the_author_meta('first_name', $author_id);
+                                            $last_name = get_the_author_meta('last_name', $author_id);
+                                            $display_name = trim($first_name . ' ' . $last_name);
+
+                                            // Usa il nome utente come fallback
+                                            if (empty($display_name)) {
+                                                $display_name = get_the_author_meta('user_login', $author_id);
+                                            }
+
+                                            // Ottieni l'URL della pagina dell'autore
+                                            $author_posts_url = get_author_posts_url($author_id);
+                                            ?>
+
+                                            <!-- Nome dell'autore cliccabile che reindirizza alla pagina dell'autore -->
+                                            <a href="<?php echo esc_url($author_posts_url); ?>" class="card_author-link">
+                                                <p class="card_author"><?php echo get_author_full_name_uppercase($author_id); ?></p>
+                                            </a>
+
+                                            <!-- Data del post -->
+                                            <p class="card_author"><?php echo get_the_date(); ?></p>
+                                        </div>
+
+                                        <div class="card_author-space">
+                                            <!-- Avatar dell'autore cliccabile che reindirizza alla pagina dell'autore -->
+                                            <a href="<?php echo esc_url($author_posts_url); ?>" class="card_author-avatar-link">
+                                                <?php echo get_avatar($author_id, 24, '', '', array('class' => 'card_author_avatar')); ?>
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
-                                <?php
-                                endwhile;
-                                wp_reset_postdata();
-                                ?>
-                    </div>
+                            </div>
+                            <?php
+                            endwhile;
+                            wp_reset_postdata();
+                            ?>
+                </div>
 
-                    <!-- Pulsante Categorie -->
-                    <div class="more-category-button">
-                        <a href="<?php echo esc_url(get_category_link($category->term_id)); ?>">
-                            <?php esc_html_e('Vedi tutto', 'universome'); ?>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="category-svg">
-                                <path
-                                    d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18a1 1 0 0 0 0-1.69L9.54 5.98A.998.998 0 0 0 8 6.82" />
-                            </svg>
-                        </a>
-                    </div>
-                    </section>
-                    <?php
+                <!-- Pulsante Categorie -->
+                <div class="more-category-button">
+                    <a href="<?php echo esc_url(get_category_link($category->term_id)); ?>">
+                        <?php esc_html_e('Vedi tutto', 'universome'); ?>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="category-svg">
+                            <path
+                                d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18a1 1 0 0 0 0-1.69L9.54 5.98A.998.998 0 0 0 8 6.82" />
+                        </svg>
+                    </a>
+                </div>
+                </section>
+                <?php
                     endif; // Fine del controllo se ci sono post
                 endif; // Fine del controllo se la categoria ha un ID
             endforeach;
@@ -245,7 +321,7 @@ $main_categories = get_main_categories();
         <div class="sidebar-content">
             <div class="swiper radio-sidebar">
                 <div class="swiper-wrapper">
-                
+
                     <div class="swiper-slide">
                         <div class="embedded-container">
                             <!-- SPOTIFY -->
@@ -265,21 +341,21 @@ $main_categories = get_main_categories();
             </div>
 
 
-           
+
         </div><!-- .sidebar-content -->
 
 
-         <!-- Pulsante per ascoltare la radio -->
-            <!-- Pulsante allineato a destra -->
-            <div class="sidebar-button-container">
-                <a href="/radio" class="sidebar-button">
-                    <?php esc_html_e('Ascoltaci!', 'universome'); ?>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="category-svg">
-                        <path
-                            d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18a1 1 0 0 0 0-1.69L9.54 5.98A.998.998 0 0 0 8 6.82" />
-                    </svg>
-                </a>
-            </div>
+        <!-- Pulsante per ascoltare la radio -->
+        <!-- Pulsante allineato a destra -->
+        <div class="sidebar-button-container">
+            <a href="/radio" class="sidebar-button">
+                <?php esc_html_e('Ascoltaci!', 'universome'); ?>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="category-svg">
+                    <path
+                        d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18a1 1 0 0 0 0-1.69L9.54 5.98A.998.998 0 0 0 8 6.82" />
+                </svg>
+            </a>
+        </div>
 
     </div><!-- .sidebar -->
     </div><!-- .w-full max-w-[1400px] -->
